b413afd55e9db3f30509e1cf3fbf57b9c01f7db5
Vincent Blouin <vincent.blouin@gmail.com>
2018-03-15 16:39:41 -0400

    init

--- index.js
+++ index.js
@@ -1,41 +1,15 @@
-var five = require("johnny-five"),
-    board = new five.Board(),
-    led,
-    toggleState = false;
+const five = require("johnny-five");
+const board = new five.Board();
 board.on("ready", function(){
-  console.log("Board ready");
-  // Create a new `button` hardware instance.
- // This example allows the button module to
- // create a completely default instance
- button = new five.Button(2);
-
- // Inject the `button` hardware into
- // the Repl instance's context;
- // allows direct command line access
- board.repl.inject({
-   button: button
- });
-
- // Button Event API
-
- // "down" the button is pressed
- // button.on("down", function() {
- //   console.log("down");
- // });
-
- // "hold" the button is pressed for specified time.
- //        defaults to 500ms (1/2 second)
- //        set
- // button.on("hold", function() {
- //   console.log("hold");
- // });
-
- // "up" the button is released
-    var nbPorteOuverte = 0;
- button.on("down", function() {
-     nbPorteOuverte++;
-     console.log("la porte s'est ouverte " + nbPorteOuverte + " fois");
- });
+    console.log("Board ready");
+    const button = new five.Button(2);
+    board.repl.inject({
+        button:button
+    });
+    let nbPorteOuverte = 0;
+    button.on("up", function(){
+        nbPorteOuverte++;
+        console.log("la porte s'est ouverte " + nbPorteOuverte + " fois")
+    });
 });
-console.log("connecting");
-
+console.log("connecting porte-loco")
