2abbd101ff504a96ac65a4c52dfabe45baeeaefb
Vincent Blouin <vincent.blouin@gmail.com>
2018-03-23 12:55:03 -0400

    http

--- index.js
+++ index.js
@@ -2,23 +2,22 @@ const five = require("johnny-five");
 const board = new five.Board();
 const http = require('http');
 const config = require('./config.json');
-board.on("ready", function(){
+board.on("ready", function () {
     console.log("Board ready");
     const button = new five.Button(2);
     board.repl.inject({
-        button:button
+        button: button
     });
     let nbPorteOuverte = 0;
     console.log(config.remoteUrl);
-    button.on("up", function(){
-        http.get(config.remoteUrl + "?date=" + new Date(), function(resp){
-            resp.on('end', function(){
+    button.on("up", function () {
+        http.get(config.remoteUrl + "?date=" + new Date(), (resp) => {
+            resp.on('end', () => {
                 console.log('request sent')
             });
-        }).on('error', function(){
-            console.log('error sending request')
-        }).on('end', function(){
-            console.log('request sent')
+
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
         });
         nbPorteOuverte++;
         console.log("la porte s'est ouverte " + nbPorteOuverte + " fois")
