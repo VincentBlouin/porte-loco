4b2eb597e4c837d2796f279dfa5dff1803798d1d
Vincent Blouin <vincent.blouin@gmail.com>
2018-03-23 13:36:38 -0400

    redis

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
-if (htmlspecialchars($_GET["date"])) {
-    echo $_GET["date"];
-}
+require "predis/autoload.php";
+PredisAutoloader::register();
+try {
+    $redis = new PredisClient();
 
-header("HTTP/1.1 200 OK");
+    // This connection is for a remote server
+    /*
+        $redis = new PredisClient(array(
+            "scheme" => "tcp",
+            "host" => "153.202.124.2",
+            "port" => 6379
+        ));
+    */
+    header("HTTP/1.1 200 OK");
+} catch (Exception $e) {
+    header("HTTP/1.1 500 INTERNAL ERROR");
+    die($e->getMessage());
+}
+if (htmlspecialchars($_GET["date"])) {
+    $redis->rpush("doorDates", $_GET["date"]);
+} else {
+    echo $redis->lpop("doorDates");
+}
