0439109f20f0b6cab5bb686e21832bb0b01bf8f5
Vincent Blouin <vincent.blouin@gmail.com>
2020-12-09 14:51:40 -0500

    sort

--- index.php
+++ index.php
@@ -106,6 +106,7 @@ if (isset($_GET["date"])) {
                 },
                 {
                     text: 'Jour',
+                    text:'formatted',
                     value: 'time'
                 },
             ],
